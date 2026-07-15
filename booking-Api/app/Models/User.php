56f89a416ac1efde203af2fd0ede3c958a3427a5
moustafagebreel <moustafagebreeel@gmail.com>
2026-07-15 16:39:55 +0300

    feat: integrate Sanctum for API token authentication in User model

--- booking-Api/app/Models/User.php
+++ booking-Api/app/Models/User.php
@@ -15,7 +15,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,
+        \Laravel\Sanctum\HasApiTokens;
 
     /**
      * Get the attributes that should be cast.
