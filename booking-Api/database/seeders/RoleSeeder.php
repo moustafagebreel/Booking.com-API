a5f26ab5e03e6149f939cf07666cc6b46be8bd0e
moustafagebreel <moustafagebreeel@gmail.com>
2026-07-15 13:12:30 +0300

    create RoleSeeder to define user roles in the database

---
+++ booking-Api/database/seeders/RoleSeeder.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        Role::create(['name' => 'Administrator']);
+        Role::create(['name' => 'Property Owner']);
+        Role::create(['name' => 'Simple User']);
+    }
+}
