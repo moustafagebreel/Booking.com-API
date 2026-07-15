8fdbe516ed48c1a55d07e6eb39707809ffc74b8a
moustafagebreel <moustafagebreeel@gmail.com>
2026-07-15 17:11:15 +0300

    feat: add GateDefineMiddleware to auth middleware group for dynamic permission handling

--- booking-Api/routes/api.php
+++ booking-Api/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Owner\PropertyController;
 use App\Http\Controllers\User\BookingController;
+use App\Http\Middleware\GateDefineMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,7 @@ Route::get('/user', function (Request $request) {
 Route::post('auth/register', RegisterController::class);
 
 
-Route::middleware('auth:sanctum')->group(function() {
+Route::middleware(['auth:sanctum', GateDefineMiddleware::class])->group(function() {
     // No owner/user grouping, for now, will do it later with more routes
     Route::get('owner/properties',
         [PropertyController::class, 'index']);
